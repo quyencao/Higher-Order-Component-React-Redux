7ec1cab178bb5070c58476a635b4e7ec7dab4a1e
Quyen <boy20195@gmail.com>
2016-09-11 16:10:39 +0700

    Add header and routes for home and resources page

---
+++ src/components/resources.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+export default () => {
+	return (
+		<div>
+			Super Special Secret Recipe
+			<ul>
+				<li>1 Eggs</li>
+				<li>1 cup Milk</li>
+				<li>Chocolate</li>
+			</ul>
+		</div>
+	);
+};
---
+++ src/index.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+
+import App from './components/app';
+import reducers from './reducers';
+import Resources from './components/resources';
+import { Router, Route , browserHistory } from 'react-router';
+
+const createStoreWithMiddleware = applyMiddleware()(createStore);
+
+ReactDOM.render(
+  <Provider store={createStoreWithMiddleware(reducers)}>
+    <Router history={ browserHistory }>
+    	<Route path="/" component={ App }>
+    		<Route path="resources" component={ Resources } />
+    	</Route>
+    </Router>
+  </Provider>
+  , document.querySelector('.container'));
