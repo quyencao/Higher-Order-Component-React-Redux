423f179e4d4007dfc93f494dcd09fe9bf9a11ba8
Quyen <boy20195@gmail.com>
2016-09-11 18:42:03 +0700

    Add Toggle Sign In Out Button By Change Application State

--- src/components/header.js
+++ src/components/header.js
@@ -1,10 +1,16 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router';
+import { connect } from 'react-redux';
+import * as actions from '../actions/index';
 
 class Header extends Component {
 
 	authButton() {
-		return <button className="btn btn-primary">Sign in</button>;
+		if(this.props.authenticated) {
+			return <button className="btn btn-primary" onClick={ () => this.props.authenticate(false) }>Sign out</button>;
+		} 
+		
+		return <button className="btn btn-primary" onClick={ () => this.props.authenticate(true) }>Sign in</button>;
 	}
 
 	render() {
@@ -26,4 +32,10 @@ class Header extends Component {
 	}
 }
 
-export default Header;
+function mapStateToProps(state) {
+	return {
+		authenticated: state.authenticated
+	}
+}
+
+export default connect(mapStateToProps, actions)(Header);
